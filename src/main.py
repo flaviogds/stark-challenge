4486b6a46847368a336e858fe1ed788d00216c69
Flavio Santos <flavio.gds@gmail.com>
2024-07-31 18:42:34 -0300

    feat: Update API documentation endpoint to /api/docs

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 from src.core.router import api_router
 
-app = FastAPI(title="StarkChallenge", description="Stark Bank Skills Test", version="0.1.0", docs_url="/docs")
+app = FastAPI(title="StarkChallenge", description="Stark Bank Skills Test", version="0.1.0", docs_url="/api/docs")
 
 app.include_router(api_router)
