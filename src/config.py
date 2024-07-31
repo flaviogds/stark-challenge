dc21e9e61e0a1386626b9724b40673efc093e83d
Flavio Santos <flavio.gds@gmail.com>
2024-07-31 09:05:49 -0300

    chore: Update actions/checkout to v4 in quality.yml and fix json.loads default values in config.py

--- src/config.py
+++ src/config.py
@@ -12,5 +12,5 @@ class Config:
     project_id = environ.get("PROJECT_ID", "")
     enviroment = environ.get("MODE", "")
     private_key = base64.b64decode(environ.get("SSL_KEY", "")).decode("utf-8")
-    transfer_account = json.loads(environ.get("TRANSFER_ACCOUNT", {}))
-    google_credentials = json.loads(environ.get("GOOGLE_CREDENTIALS", {}))
+    transfer_account = json.loads(environ.get("TRANSFER_ACCOUNT", "{}"))
+    google_credentials = json.loads(environ.get("GOOGLE_CREDENTIALS", "{}"))
